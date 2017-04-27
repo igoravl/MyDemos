16274da0c879564209eaf1e2bfa627f233cee27d
Igor Abade (MSA) <igoravl@hotmail.com>
2017-04-27 17:13:42 -0300

    Complete DSC script

--- 2017-04-27 - Meetup Delivering Software/WebServerVm.ps1
+++ 2017-04-27 - Meetup Delivering Software/WebServerVm.ps1
@@ -1,14 +1,42 @@
-Configuration BuildVM {
+param
+(
+    [Parameter(Mandatory=$true)]
+    [string] $VmName,
+
+    [string] $HostName = 'localhost',
+
+    [int]    $ProcessorCount = 1,
+
+    [long]   $MemorySize = 2GB,
+
+    [string] $VmAdminUserName,
+    
+    [securestring] $VmAdminPassword
+ 
+)
+
+Configuration HostConfiguration {
 
     param
     (
-        [Parameter()]
+        [Parameter(Mandatory=$true)]
+        [string]
+        $VmName,
+
         [string]
         $HostName = 'localhost',
 
-        [Parameter(Mandatory=$true)]
+        [int]
+        $ProcessorCount = 1,
+
+        [long]
+        $MemorySize = 2GB,
+
         [string]
-        $VmName
+        $VmAdminUserName,
+    
+        [securestring]
+        $VmAdminPassword
     )
 
     Import-DscResource -ModuleName PsDesiredStateConfiguration
@@ -16,13 +44,99 @@ Configuration BuildVM {
 
     Node $HostName {
 
-        xVHD MyVHD {
+        File VhdDir {
+            DestinationPath = "D:VHD\$VmName"
+            Type = 'Directory'
+        }
+
+        xVHD VmVhd {
             Ensure      = 'Present'
-            Name        = 'Drive-C.vhd'
-            Path        = 'D:VHD'
+            Name        = "Drive-C.vhd"
+            Path        = "D:\VHD\$VmName"
             Generation  = 'vhd'
-            ParentPath  = 'D:\VHD\Template\en-windows-server-2016-datacenter.vhd' 
-            MaximumSizeBytes     = 127GB
+            ParentPath  = 'D:\VHD\Template\en-windows-server-2016-datacenter-base.vhd' 
+            DependsOn = '[File]VhdDir'
+        }
+
+        xVMHyperV CreateVm {
+            Ensure = 'Present'
+            DependsOn = '[xVHD]VmVhd' ## Ensures the that VHD is created first
+            Name = $VmName
+            VhdPath = "D:\VHD\$VmName\Drive-C.vhd" ## To use the VHD that was created above
+            SwitchName = 'Internal Virtual Switch' ## Use an already built virtual switch called LabSwitch
+            State = 'Running' ## When finished, start up the VM
+            Path = "D:\Hyper-V" ## Place the VM on the file system at C:VMs
+            Generation = 1 ## This will be an older generation VM
+            StartupMemory = $MemorySize ## It will have 2GB of memory
+            ProcessorCount = $ProcessorCount ## It will have a single processor
+            WaitForIP = $true
+            RestartIfNeeded = $true
         }
     }
 }
+
+Configuration VmConfiguration {
+
+    param(
+        [string]
+        $VmName,
+
+        [string]
+        $VmIpAddress
+    )
+
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -ModuleName xComputerManagement
+
+    Node $VmIpAddress {
+
+        xComputer VmName {
+            Name = $VmName
+        }
+
+        WindowsFeature IIS 
+        { 
+            Ensure = 'Present'
+            Name = 'Web-Server'
+        } 
+
+        WindowsFeature IisManager 
+        { 
+            Ensure = 'Present'
+            Name = 'Web-Mgmt-Console'
+            DependsOn = '[WindowsFeature]IIS'
+        } 
+
+        WindowsFeature WebManagementService
+        {
+            Ensure = "Present"
+            Name = "Web-Mgmt-Service"
+            DependsOn = '[WindowsFeature]IIS'
+        }
+
+        WindowsFeature ASP 
+        { 
+            Ensure = 'Present'
+            Name = 'Web-Asp-Net45'
+            DependsOn = '[WindowsFeature]IIS'
+        }
+
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $True
+        }
+    }
+}
+
+$vmcreds = New-Object System.Management.Automation.PSCredential($VmAdminUserName, $VmAdminPassword)
+
+HostConfiguration @PSBoundParameters
+Start-DscConfiguration -Path .\HostConfiguration -Force -Wait
+
+$ipAddress = (Get-VMNetworkAdapter -VMName $VmName).IpAddresses[0]
+Set-Item wsman:\localhost\Client\TrustedHosts -Value $ipAddress -Force
+
+$cimSession = New-CimSession -ComputerName $ipAddress -Credential $vmcreds
+Set-DscLocalConfigurationManager -CimSession $cimSession -Path .\VmConfiguration
+
+VmConfiguration -VmName $VmName -VmIpAddress $ipAddress
+Start-DscConfiguration -Path .\VmConfiguration -Force -Wait -CimSession $cimSession
