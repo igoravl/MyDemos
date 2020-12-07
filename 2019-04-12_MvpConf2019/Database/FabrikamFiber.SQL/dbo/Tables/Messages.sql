d78ee6f0f7b6104dd62e8e828a3160d0618d556e
Igor Abade <igor.abade@cloudmotion.com.br>
2020-12-07 01:09:06 -0300

    MVP Conf 2020 - commit inicial

--- 2019-04-12_MvpConf2019/Database/FabrikamFiber.SQL/dbo/Tables/Messages.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Messages] (
-    [ID]          INT            IDENTITY (1, 1) NOT NULL,
-    [Sent]        DATETIME       NOT NULL,
-    [Description] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_dbo.Messages] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
